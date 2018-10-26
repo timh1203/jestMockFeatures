188d6ba7c61dbf0368d071a211987f3a30f863b5
Timothy Hoang <timh1203@gmail.com>
2018-10-26 09:24:42 -0400

    Add jest.mock method to mock

--- app.test.js
+++ app.test.js
@@ -2,11 +2,28 @@ const app = require('./app.js');
 const math = require('./math.js');
 
 // METHOD 1: jest.fn to mock a function
-math.add = jest.fn();
-math.subtract = jest.fn();
+// math.add = jest.fn();
+// math.subtract = jest.fn();
 
-describe('Method 1: jest.fn', () => {
-  it('should run without crashing', () => { })
+// describe('Method 1: jest.fn', () => {
+//   it('calls math.add', () => {
+//     app.doAdd(1, 2);
+//     expect(math.add).toHaveBeenCalled();
+//     expect(math.add).toHaveBeenCalledTimes(1);
+//     expect(math.add).toHaveBeenCalledWith(1, 2);
+//   })
+//   it('calls math.subtract', () => {
+//     app.doSubtract(3, 1);
+//     expect(math.subtract).toHaveBeenCalled();
+//     expect(math.subtract).toHaveBeenCalledTimes(1);
+//     expect(math.subtract).toHaveBeenCalledWith(3, 1);
+//   })
+// })
+
+// METHOD 2: jest.mock to mock a module
+jest.mock('./math.js');
+
+describe('Method 2: jest.mock', () => {
   it('calls math.add', () => {
     app.doAdd(1, 2);
     expect(math.add).toHaveBeenCalled();
@@ -21,5 +38,4 @@ describe('Method 1: jest.fn', () => {
   })
 })
 
-// METHOD 2: jest.mock to mock a module
 // METHOD 3: jest.spyOn to spy or mock a function
